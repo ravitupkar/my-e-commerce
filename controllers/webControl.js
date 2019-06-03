81076427321f44bd8f6e9883b08c616a1490f4b0
ravitupkar <ravitupkar01@gmail.com>
2019-06-03 14:51:00 +0530

    first commit

---
+++ controllers/webControl.js
@@ -0,0 +1,47 @@
+exports.login = (req, res) =>{
+    res.render('login.ejs');
+}
+
+exports.register = (req, res) =>{
+    res.render('register.ejs');
+}
+
+exports.about  = (req, res) =>{
+    res.render('about.ejs');
+}
+
+exports.services  = (req, res) =>{
+    res.render('services.ejs');
+}
+
+exports.products  = (req, res) =>{
+    res.render('products.ejs');
+}
+
+exports.productsDetails  = (req, res) =>{
+    res.render('products-details.ejs');
+}
+
+exports.blog  = (req, res) =>{
+    res.render('blog.ejs');
+}
+
+exports.blogDetails  = (req, res) =>{
+    res.render('blog-details.ejs');
+}
+
+exports.contact  = (req, res) =>{
+    res.render('contact.ejs');
+}
+
+exports.about  = (req, res) =>{
+    res.render('about.ejs');
+}
+
+exports.about  = (req, res) =>{
+    res.render('about.ejs');
+}
+
+exports.home  = (req, res) =>{
+    res.render('index.ejs');
+}
